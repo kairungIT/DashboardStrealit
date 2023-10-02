453904711590cc65e43f43c1c882d03d42512e64
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-10-02 14:11:28 +0700

    .commit

--- Home.py
+++ Home.py
@@ -18,7 +18,7 @@ def load_lottieurl(url: str):
 
 
 lottie_url_hello = "https://lottie.host/3f647b41-61bf-4d39-93c3-0433420604cc/8NtmEbAWmO.json"
-lottie_url_download = "https://assets4.lottiefiles.com/private_files/lf30_t26law.json"
+lottie_url_download = "https://lottie.host/290016a6-d650-4a32-85c7-4d77b0a892ca/4rNL8XZeZt.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 lottie_download = load_lottieurl(lottie_url_download)
 
