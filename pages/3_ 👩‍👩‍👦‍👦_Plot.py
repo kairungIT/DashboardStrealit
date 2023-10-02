f6cf4503dba7ffba5b3ad4dbc0d3ca16c0e5a689
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-10-02 14:58:46 +0700

    .commit

---
+++ pages/3_ 👩‍👩‍👦‍👦_Plot.py
@@ -0,0 +1,16 @@
+import json
+import time
+import requests
+import streamlit as st
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie_spinner
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+lottie_url_hello = "https://lottie.host/290016a6-d650-4a32-85c7-4d77b0a892ca/4rNL8XZeZt.json"
+lottie_hello = load_lottieurl(lottie_url_hello)
+st_lottie(lottie_hello, key="hello")
