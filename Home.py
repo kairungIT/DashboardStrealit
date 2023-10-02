d3c66d38456797b63bc1cf3a6ee711f04a2d843d
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-10-02 14:21:09 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,7 +2,14 @@ import json
 import time
 import requests
 import streamlit as st
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
 
+st.write("# Welcome to Streamlit! 👋")
+
+st.sidebar.success("Select a demo above.")
 
 st.header("kairung")
 st.balloons()
