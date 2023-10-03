d9b1ad0d79204acdf2b650d4cb921bfe35643190
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-10-03 15:42:59 +0700

    .commit

--- pages/4_New.py
+++ pages/4_New.py
@@ -8,5 +8,8 @@ st.header("Show Data Index Price")
 df=pd.read_csv("./data/stock_index_price.csv")
 st.write(df.head(10))
 
+st.line_chart(
+   df, x="stock_index_price", y=["interest_rate", "unemploment_rate"], color=["#FF0000", "#0000FF"]  # Optional
+)
 
 
