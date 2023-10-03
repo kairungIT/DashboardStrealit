996111ae206a7ee76e805a4f4a508408dcae8229
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-10-03 14:56:16 +0700

    .commit

--- pages/2_ 👩‍👩‍👦‍👦_AboutMe.py
+++ pages/2_ 👩‍👩‍👦‍👦_AboutMe.py
@@ -11,7 +11,7 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-lottie_url_hello = "https://lottie.host/3f647b41-61bf-4d39-93c3-0433420604cc/8NtmEbAWmO.json"
+lottie_url_hello = "https://lottie.host/acd0c339-c8cb-4796-930f-5c7844bbbc1c/eiEBu3mxcA.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 st_lottie(lottie_hello, key="hello")
 
